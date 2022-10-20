405b784e2f5fca65b3c0709f1ae270703de9097c
POSEIDONStr <116230332+POSEIDONStr@users.noreply.github.com>
2022-10-20 01:18:04 +0000

    Aprendemos a ecibir de 3 maneiras difentes

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ idade = 19
 #Exibir o meu nome (que esta dentro da variavel nome)
 print(nome)
 print(idade)
+#Quando Quiser exibir a frase "Minha idade é " completando  com o conteudo da variavel idade
+print("Meu nome é "+nome)
+print ("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
 
-
-
-
+#Quando quiser exibir "Meu nome é ... e tenho ... anos.." trocando pelas variaveis nome e idade 
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
 
 '''
 Comentario em bloco
