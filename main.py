af22fba130386a1d74d8e49ce6e1fe21cc30a9cf
POSEIDONStr <116230332+POSEIDONStr@users.noreply.github.com>
2022-10-20 00:53:21 +0000

    aprendi a criar variaveis e exibir variaveis na tela

--- main.py
+++ main.py
@@ -3,6 +3,18 @@
 # print() = comando de saida 
 print("Alo Mundo!")
 
+# Quando quiser guardar uma String! (frases)
+nome = "Gabriel Alves"
+#Quando quiser guardar um numeor interiro
+idade = 19
+
+#Exibir o meu nome (que esta dentro da variavel nome)
+print(nome)
+print(idade)
+
+
+
+
 
 '''
 Comentario em bloco
