0af8ec9237b22147f1043b6dd488d945c6bed866
Gabriel Alves <116230332+POSEIDONStr@users.noreply.github.com>
2022-10-22 19:07:03 +0000

    aprendendo

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ print("Alo Mundo!")
 nome = "Gabriel Alves"
 #Quando quiser guardar um numeor interiro
 idade = 19
+valor = 40
+endereco = "rua mooca paulista"
+email = "lokipo@teste.com.br"
 
 #Exibir o meu nome (que esta dentro da variavel nome)
 print(nome)
 print(idade)
+print(valor)
+print(endereco)
+print(email)
 #Quando Quiser exibir a frase "Minha idade é " completando  com o conteudo da variavel idade
 print("Meu nome é "+nome)
 print ("Minha idade é "+str(idade))
 print(f"Minha idade é {idade}\n")
 print("Minha idade é {}".format(idade))
+print("valor a pagar é "+str(valor))
+print("o endereco é "+endereco)
+print("qual o seu email: "+email)
+
 
 #Quando quiser exibir "Meu nome é ... e tenho ... anos.." trocando pelas variaveis nome e idade 
 print("Meu nome é {} e tenho {} anos".format(nome,idade))
