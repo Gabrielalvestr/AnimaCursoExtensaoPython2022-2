507f4e6d403f023a5867561bc5a7ae3b9ed41e92
Gabriel Alves <116230332+POSEIDONStr@users.noreply.github.com>
2022-10-26 23:13:37 +0000

    Foi ensinado comando de entrada input() int(input()

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-# Meu Primeiro projeto Python!!!
-#
-# print() = comando de saida 
-print("Alo Mundo!")
+# comando input(): quero permitir que o usuario digite algo...
+nome = input("Digite seu nome: ")
+# comando de saida..exibir na tela 
+idade = int(input("Digite sua idade: "))
 
-# Quando quiser guardar uma String! (frases)
-nome = "Gabriel Alves"
-#Quando quiser guardar um numeor interiro
-idade = 19
-valor = 40
-endereco = "rua mooca paulista"
-email = "lokipo@teste.com.br"
 
-#Exibir o meu nome (que esta dentro da variavel nome)
 print(nome)
 print(idade)
-print(valor)
-print(endereco)
-print(email)
-#Quando Quiser exibir a frase "Minha idade é " completando  com o conteudo da variavel idade
-print("Meu nome é "+nome)
-print ("Minha idade é "+str(idade))
-print(f"Minha idade é {idade}\n")
-print("Minha idade é {}".format(idade))
-print("valor a pagar é "+str(valor))
-print("o endereco é "+endereco)
-print("qual o seu email: "+email)
 
+print(f"Boa noite, seu nome é {nome}")
 
-#Quando quiser exibir "Meu nome é ... e tenho ... anos.." trocando pelas variaveis nome e idade 
-print("Meu nome é {} e tenho {} anos".format(nome,idade))
+#e se eu quisesse mostrar o dobro da idade informada?
+dobro = idade * 2 
+print("O dobro da idade informada é {}".format(dobro))
 
-'''
-Comentario em bloco
-'''
